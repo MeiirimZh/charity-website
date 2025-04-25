1ad1487836dc3d0cbeb7906b7396ff7d57968c14
MeiirimZh <zh.meyirim@gmail.com>
2025-04-25 16:11:36 +0500

    started working on sign in

---
+++ sign.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Charity</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/form.css">
+</head>
+<body>
+    <?php
+        require_once "blocks/header.html";
+    ?>
+    <main>
+        <form class="sign_in" action="" method="POST">
+            <h2>Email</h2>
+            <input type="email" name="email" placeholder="Enter...">
+            <h2>Password</h2>
+            <input type="password" name="password" placeholder="Enter...">
+            <input type="submit" value="Confirm">
+        </form>
+    </main>
+</body>
+</html>
